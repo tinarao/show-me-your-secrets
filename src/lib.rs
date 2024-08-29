29ad55554f5956326e44a6ad33f35ba986c88313
tinarao <golovindanila1613@gmail.com>
2024-08-29 15:41:30 +0300

    some refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -1,41 +1,42 @@
-mod parser;
+mod smys;
 
 #[cfg(test)]
 mod tests {
     use std::fs;
 
     use super::*;
+    use smys::Parser;
 
     #[test]
     fn env_file_exists() {
         if fs::metadata("./.env").is_ok() {
-            let is_exists = parser::ok();
+            let is_exists = Parser::ok();
             assert_eq!(is_exists, true);
         } else {
-            let is_exists = parser::ok();
+            let is_exists = Parser::ok();
             assert_eq!(is_exists, false);
         }
     }
 
     #[test]
     fn get_known_vars() {
-        if !parser::ok() {
+        if !Parser::ok() {
             println!(".env file does not exist!")
         };
 
-        assert_eq!(parser::get("FOO"), "BAR");
-        assert_eq!(parser::get("BAR"), "BAZ");
-        assert_eq!(parser::get("BAZ"), "BAT");
+        assert_eq!(Parser::get("FOO"), "BAR");
+        assert_eq!(Parser::get("BAR"), "BAZ");
+        assert_eq!(Parser::get("BAZ"), "BAT");
     }
 
     #[test]
     fn get_unknown_vars() {
-        if !parser::ok() {
+        if !Parser::ok() {
             println!(".env file does not exist!")
         };
 
-        assert_eq!(parser::get("UNKNOWN_ONE"), String::new());
-        assert_eq!(parser::get("WE_DONT_HAVE_THIS_ONE"), String::new());
-        assert_eq!(parser::get("NOTHING"), String::new());
+        assert_eq!(Parser::get("UNKNOWN_ONE"), String::new());
+        assert_eq!(Parser::get("WE_DONT_HAVE_THIS_ONE"), String::new());
+        assert_eq!(Parser::get("NOTHING"), String::new());
     }
 }
